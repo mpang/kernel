46ea1ce19865a21c8f43f8a1148f55908e4a7dcf
Meng Pang <pangmeng0218@gmail.com>
2014-01-22 14:09:35 -0800

    use flag instead of function to check open devices

--- c/di_calls.c
+++ c/di_calls.c
@@ -1,7 +1,7 @@
 #include <xeroskernel.h>
 #include <kbd.h>
 
-int other_device_open(void);
+int other_device_open = 0;
 
 void di_init(void)
 {
@@ -25,7 +25,7 @@ int di_open(struct pcb *process, int device_no)
   }
 
   // only a single device can be open at a time
-  if (other_device_open()) {
+  if (other_device_open) {
     return -1;
   }
 
@@ -38,6 +38,7 @@ int di_open(struct pcb *process, int device_no)
         return -1;
       }
 
+      other_device_open = 1;
       process->fd_table[i] = device_no;
       return i;
     }
@@ -60,6 +61,7 @@ int di_close(struct pcb *process, int fd)
     return -1;
   }
 
+  other_device_open = 0;
   int result = device_table[process->fd_table[fd]].close();
   process->fd_table[fd] = -1;
   return result;
@@ -102,22 +104,4 @@ int di_ioctl(struct pcb *process, int fd, unsigned long command, unsigned int ar
   }
 
   return device_table[process->fd_table[fd]].ioctl(command, args);
-}
-
-// return 1 if other device is opened, 0 otherwise
-int other_device_open(void)
-{
-  int i;
-  for (i = 0; i < PCB_TABLE_SIZE; i++) {
-    if (pcb_table[i].state != STOPPED) {
-      int j;
-      for (j = 0; j < FD_TABLE_SIZE; j++) {
-        if (pcb_table[i].fd_table[j] > -1) {
-          return 1;
-        }
-      }
-    }
-  }
-
-  return 0;
 }
